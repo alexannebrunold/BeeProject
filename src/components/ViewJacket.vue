5102ebb93713365c61d27f0350fac01d1a6c392f
Alexanne Brunold <alexanne.brunold@hetic.net>
2020-06-30 12:15:34 +0200

    creation comment viewjacket and creation vue personnalisation

---
+++ src/components/ViewJacket.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="viewJacket">
+		<div>+ -</div>
+		<div>
+			<img src="@/assets/img/Veste_Jean_Bleached.jpg" alt="" />
+		</div>
+		<div>
+			<img src="@/assets/img/leftArrow.svg" alt="" />
+			<img src="@/assets/img/rightArrow.svg" alt="" />
+		</div>
+		<div>
+			Been
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Button",
+	props: {},
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/Personnalisation.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<ViewJacket />
+		<Footer />
+	</div>
+</template>
+
+<script>
+import ViewJacket from "@/components/ViewJacket.vue"
+import Footer from "@/components/Footer.vue"
+export default {
+	name: "Home",
+	components: {
+		ViewJacket,
+		Footer,
+	},
+}
+</script>
